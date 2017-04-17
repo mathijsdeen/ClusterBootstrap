80af61b39d5789a567655c15e847248a7f7048c4
mathijsdeen <mathijsdeen@gmail.com>
2017-04-17 23:00:50 +0200

    solve as.factor() bug in clusjackglm

--- R/clusbootglm_internals.R
+++ R/clusbootglm_internals.R
@@ -24,7 +24,6 @@ cbglm.doplot<-function(ci_table,coefs,method,show.intercept){
   }
 }
 
-
 cbglm.signif.check<-function(ci_row){
   if(ci_row[1]<0 && ci_row[2]<0) return(1)
   else if(ci_row[1]>0 && ci_row[2]>0) return(1)
@@ -54,12 +53,13 @@ clusjackglm <- function (model, data, clusterid, family = gaussian) {
   for (i in 1:nc) {
     obs <- unlist(Obsno[-i])
     jackcoef <- coef(glm(model, family=family, data = data[obs,]))
-    ifelse(length(jackcoef)==p, coefs[i, ] <- as.vector(jackcoef), coefs[i,] <- rep(NA,p))
+    #ifelse(length(jackcoef)==p, coefs[i, ] <- as.vector(jackcoef), coefs[i,] <- rep(NA,p))
+    ifelse(length(jackcoef)==p, coefs[i,] <- as.vector(jackcoef), coefs[i,which(names(res.or$coef) %in% names(jackcoef))] <- jackcoef)
   }
   uu <- -sweep(coefs,2,colMeans(coefs,na.rm = T), FUN="-")
   acc<-rep(NA,p)
   for(i in 1:p){
-    acc[i] <- sum(uu[,i] * uu[,i]* uu[,i])/(6 * (sum(uu[,i] * uu[,i]))^1.5)
+    acc[i] <- sum(uu[,i] * uu[,i]* uu[,i],na.rm = T)/(6 * (sum(uu[,i] * uu[,i],na.rm=T))^1.5)
   }
   return(acc)
 }

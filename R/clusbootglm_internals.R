e92ac75a5880d3d77f58fc444758d6e633e0faf7
Mathijs Deen <mathijsdeen@gmail.com>
2020-02-20 18:43:03 +0100

    remove old commented line

--- R/clusbootglm_internals.R
+++ R/clusbootglm_internals.R
@@ -4,7 +4,6 @@ clusbootglm_sample_glm <-function(f, i, Obsno, model, family, data, p, res.or){
   coef <- rep(NA,p) #added
   bootcoef <- tryCatch(coef(glm(model, family = family, data = data[obs,])),
                       warning=function(x) rep(as.numeric(NA),p))
-  #ifelse(length(bootcoef)==p, bootcoef <- as.vector(bootcoef), bootcoef <- rep(NA,p))
   ifelse(length(bootcoef)==p, coef <- as.vector(bootcoef), coef[which(names(res.or$coef) %in% names(bootcoef))] <- bootcoef)
   return(coef)
 }

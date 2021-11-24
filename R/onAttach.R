52cba174f638717971c09129070e389ae1daf570
Mathijs Deen <mathijsdeen@gmail.com>
2021-11-24 15:56:19 +0100

    activate .onAttach packageStartupMessage temporarily

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,3 +1,3 @@
-#.onAttach <- function(libname, pkgname) {
-#  packageStartupMessage("This is a development version of ClusterBootstrap!")
-#}
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("This is an experimental version of ClusterBootstrap! \nFor the latest stable release, visit https://cran.r-project.org/package=ClusterBootstrap")
+}

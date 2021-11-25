f88f27130909a42ac74ae758f054e5c0a9fd665b
Mathijs Deen <mathijsdeen@gmail.com>
2021-11-25 12:34:12 +0100

    create separate file for emmeans

--- R/predict.clusbootglm.R
+++ R/predict.clusbootglm.R
@@ -47,33 +47,3 @@ predict.clusbootglm <- function(object, stat = mean, newdata = NULL, interval = 
   }
   return(out)
 }
-
-#' @title Calculate estimated marginal means for a cluster bootstrap GLM
-#' @description Returns estimated marginal means of a \code{clusbootglm} object. This is an experimental approach to the old \code{emm} function.
-#' @param object Object of class \code{clusbootglm}.
-#' @param confint.level Level of the confidence interval. Must be between 0 and 1. Defaults to 0.95.
-#' @return \code{emmeans} returns estimated marginal means for a cluster bootstrap GLM. Does not work with \code{plot.clusbootemm} yet.
-#' @author Mathijs Deen
-#' @examples 
-#' \dontrun{
-#' medication <- medication[medication$time %% 1 == 0,]
-#' medication$time <- as.factor(medication$time)
-#' set.seed(1)
-#' model.1 <- clusbootglm(pos~time*treat, clusterid = id, data = medication)
-#' emmeans(object = model.1)}
-#' @importFrom dplyr arrange
-#' @importFrom magrittr %>%
-#' @importFrom rlang syms
-#' @importFrom rlang !!!
-#' @importFrom rlang .data
-#' @export
-emmeans <- function(object, confint.level = 0.95) {
-  df <- data.frame(unique(object$data[ , all.vars(object$model[-2])]))
-  names(df) <- all.vars(object$model[-2])
-  emms <- predict.clusbootglm(object, newdata = df, interval = "confidence")
-  emms <- cbind(df, emms)
-  colnames(emms)[ncol(emms)] <- "emmean"
-  emms <- data.frame(emms)
-  emms <- emms %>% arrange(!!!syms(colnames(.data)))
-  return(emms)
-}

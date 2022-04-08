1ace2ea4be04ec1097c25b96cba7f3f574f54c43
Mathijs Deen <mathijsdeen@gmail.com>
2022-04-08 16:39:35 +0200

    add arguments and optimize code

--- R/plot.clusbootemm.R
+++ R/plot.clusbootemm.R
@@ -5,6 +5,9 @@
 #' @param between between-subjects variable.
 #' @param pch point character. Length must be equal to the number of between-subjects levels.
 #' @param lty linetype. Length must be equal to the number of between-subjects levels.
+#' @param pcol point color. Length must be equal to the number of between-subjects levels.
+#' @param lcol line color. Length must be equal to the number of between-subjects levels.
+#' @param ylim limits of the y axis. If omitted, it will be based on the lowest and highest values within the confidence intervals of the estimated marginal means.
 #' @param ylab label for y-axis.
 #' @param xlab label for x-axis.
 #' @param ... other arguments to be passed to the \code{plot} function (see \code{\link{par}}). 
@@ -14,36 +17,45 @@
 #' medication <- medication[medication$time %% 1 == 0,]
 #' medication$time_f <- as.factor(medication$time)
 #' set.seed(1)
-#' model.1 <- clusbootglm(pos~time_f*treat, clusterid=id, data=medication)
+#' model.1 <- clusbootglm(pos~time_f*treat, clusterid = id, data = medication)
 #' emm.1 <- emm(object = model.1)
-#' plot(x = emm.1, within = time_f, between = treat, pch = c(15,17), lty = c(1,2))}
+#' plot(x = emm.1, within = time_f, between = treat, pch = c(15,17), lty = c(1,2), 
+#'      lcol = c("blue", "red"), pcol = c("blue","red"), )}
 #' @importFrom graphics plot points lines arrows
+#' @importFrom methods hasArg
 #' @export
-plot.clusbootemm <- function(x, within, between, pch, lty, ylab="Estimated marginal mean", xlab="Within subject", ...){
+plot.clusbootemm <- function(x, within, between, pch, lty, pcol, lcol, ylim,
+                             ylab="Estimated marginal mean", xlab="Within subject", ...){
   object <- x
   arguments <- as.list(match.call())
   grid <- object$grid
   within <- eval(arguments$within, grid)
-  between <- eval(arguments$between, grid)
+  between <- eval(arguments$between, grid) #NULL if not specified
   if(is.factor(within)) within <- as.numeric(levels(within))[within]
-  if(is.null(between)) between <- rep(1, length(within))
-  if(length(unique(between)) != length(pch) | length(unique(between)) != length(lty)){
-    stop(sprintf("Arguments pch and lty should be of length %d",length(unique(between))), call.=FALSE)
+  if(is.null(between)) between <- rep(1, length(within)) 
+  if(!hasArg(pch)) pch <- rep(1,length(unique(between)))
+  if(!hasArg(lty)) lty <- rep(1,length(unique(between)))
+  if(!hasArg(lcol)) lcol <- rep("black",length(unique(between)))
+  if(!hasArg(pcol)) pcol <- rep("black",length(unique(between)))
+  if(!hasArg(ylim)) ylim <- c(min(grid$lower.CL),max(grid$upper.CL))
+  if(length(unique(between)) != length(pch) | length(unique(between)) != length(lty) |
+     length(unique(between)) != length(lcol) | length(unique(between)) != length(pcol)){
+    stop(sprintf("Specified arguments pch, lty, lcol and pcol should be of length %d",
+                 length(unique(between))), call.=FALSE)
   }
   emmean <- grid$emmean
-  ylims <- c(min(grid$lower.CL),max(grid$upper.CL))
   for(i in unique(between)){
     m <- which(unique(between) %in% i)
     if(m==1) {
-      plot(within[between==i],emmean[between==i], ylim=ylims, pch=pch[m], 
-           xlab=xlab, ylab=ylab, ...)
+      plot(within[between==i],emmean[between==i], ylim=ylim, pch=pch[m], 
+           xlab=xlab, ylab=ylab, col=pcol[m])
     }else{
-      points(within[between==i],emmean[between==i], pch=pch[m])
+      points(within[between==i],emmean[between==i], pch=pch[m], col=pcol[m])
     }
-    lines(within[between==i],emmean[between==i], lty=lty[m])
-    for(i in 1:length(within)){
-      arrows(within[i],grid[i,]$emmean,within[i],grid[i,]$lower.CL,angle=90,length=.05)
-      arrows(within[i],grid[i,]$emmean,within[i],grid[i,]$upper.CL,angle=90,length=.05)
-    }
+    lines(within[between==i],emmean[between==i], lty=lty[m], col=lcol[m])
+    arrows(within[between==i],grid$emmean[between==i], within[between==i],
+           grid$lower.CL[between==i],angle=90,length=.05, col=lcol[m])
+    arrows(within[between==i],grid$emmean[between==i], within[between==i],
+           grid$upper.CL[between==i],angle=90,length=.05, col=lcol[m])
   }
 }

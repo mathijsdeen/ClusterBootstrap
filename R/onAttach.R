53cfb7e468a423fc6581bf4a6d9a8e0840595341
mathijsdeen <mathijsdeen@gmail.com>
2026-03-10 14:27:24 +0100

    minor upgrade

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,3 +1,3 @@
-#.onAttach <- function(libname, pkgname) {
-#  packageStartupMessage("This is a development version of ClusterBootstrap!")
-#}
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("This is a development version of ClusterBootstrap!")
+}
